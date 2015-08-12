bd83eb14d4977b423b63cff67b26c0d8d8d919ee
Nikko Gladkko <nikkogladkko@MacBook-Pro-Nikko.local>
2015-08-12 23:08:50 +0500

    first buid

---
+++ beaconRadar/beaconRadar/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  beaconRadar
+//
+//  Created by Nikko Gladkko on 12.08.15.
+//  Copyright (c) 2015 TNADZOR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
